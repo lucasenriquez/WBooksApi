13f40e66bc1ac089b0b53eeda74e1aae53bd23f2
lucasenriquez <lucas.enriquez@widergy.com>
2021-03-11 14:49:12 -0300

    Correct Rubocop Offenses

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < Devise::SessionsController
-
   respond_to :json
   before_action :rewrite_param_names, only: [:create]
 
@@ -10,7 +9,7 @@ class SessionsController < Devise::SessionsController
   def respond_to_on_destroy
     head :no_content
   end
-  
+
   def rewrite_param_names
     request.params[:user] =
       { email: request.params[:session][:email], password: request.params[:session][:password] }
