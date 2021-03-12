3f361d7fdee06c569ce189620fea3a3727a92eac
lucasenriquez <lucas.enriquez@widergy.com>
2021-03-12 12:44:56 -0300

    Changesin Book migration

--- db/migrate/20210312140240_create_books.rb
+++ db/migrate/20210312140240_create_books.rb
@@ -1,9 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
-  def change
+  def up
     create_table :books do |t|
       t.string :genre, null: false
       t.string :author, null: false
-      t.string :image, null: false
+      t.string :image_url, null: false
       t.string :title, null: false
       t.string :publisher, null: false
       t.string :year, null: false
