f83bdd9aa33d2adccd01f886e105bd46de716547
Lucas Enriquez <lucas.enriquez@widergy.com>
2021-03-29 22:06:31 -0300

    Create SuggestionController test

---
+++ spec/controllers/book_suggestion_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe BookSuggestionController, type: :controller do
+
+  describe 'POST #create' do
+    context 'When creating a book suggestion' do
+      let!(:book_suggestion) { create(:book_suggestion) }
+
+      subject do
+        post :create
+      end
+
+      it 'responds with the rent json' do
+        expected = book_suggestion.to_json
+        expect(response.body.to_json) =~ JSON.parse(expected)
+      end
+      
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'When a guest creates a book suggestion' do
+      let!(:book_suggestion) { create(:book_suggestion, user: nil) }
+
+      subject do
+        post :create
+      end
+
+      it 'responds with the rent json' do
+        expected = book_suggestion.to_json
+        expect(response.body.to_json) =~ JSON.parse(expected)
+      end
+      
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+end
